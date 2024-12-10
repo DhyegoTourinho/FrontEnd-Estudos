8398a8ef8036a0f3d2fc41b2030f6c536c466a79
Dhyego Tourinho <Dhyego.tourinho@discente.ufg.br>
2024-12-10 16:19:17 -0300

    Finalizacao do projeto 'Inegavel'

--- Projects/Inegavel/inegavel/src/components/Buttons.jsx
+++ Projects/Inegavel/inegavel/src/components/Buttons.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-let tamanho = 1;
+let size = 1;
 
 const DeclineButton = () => {
     
     const [position, setPosition] = useState({top: 0, left:0, tam:1});
     
     const RandomPosition = () => {
-      tamanho++;
-      setPosition({top:Math.random() * (Math.random() > 0.5 ? -400 : 400), left:Math.random() * (Math.random() > 0.5 ? -400 : 400), tam:0.1 * tamanho + 1});
+      size++;
+      setPosition({top:Math.random() * (Math.random() > 0.5 ? -400 : 400), left:Math.random() * (Math.random() > 0.5 ? -400 : 400), tam:0.1 * size + 1});
     }
 
   return (
--- Projects/Inegavel/inegavel/src/components/Title.jsx
+++ Projects/Inegavel/inegavel/src/components/Title.jsx
@@ -2,7 +2,7 @@ const Title = () => {
   return (
     <div>
         <h1>
-            Convite para maratona de No Time To Relax
+            Convite para maratona de Mount
         </h1>
     </div>
   )
